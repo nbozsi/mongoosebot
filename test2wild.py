f5039cbea6b404799ba1186969b50f05985ffcfa
nbozsi <50334026+nbozsi@users.noreply.github.com>
2020-06-09 21:48:29 +0200

    Update test2wild.py

--- test2wild.py
+++ test2wild.py
@@ -58,7 +58,7 @@ async def ping(ctx):
 @client.command(pass_context=True)
 async def mitigyak(ctx):
     """Segít eldönteni mit igyál."""
-    italok=[":wine_glass:Bort:wine_glass:",":sake:Pálinkát:sake:",":beer:Sört:beers:",":heart_eyes:Kevertet:heart_eyes:",":man_gesturing_no:Vodkát ne:man_gesturing_no:",":deer:Jégert:deer:",":sake:Tátrateát:sake:",":champagne:Pezsgőt:champagne_glass:",":whisky:Whiskyt:whisky:"]
+    italok=[":wine_glass:Bort:wine_glass:",":sake:Pálinkát:sake:",":beer:Sört:beers:",":heart_eyes:Kevertet:heart_eyes:",":man_gesturing_no:Vodkát ne:man_gesturing_no:",":deer:Jégert:deer:",":sake:Tátrateát:sake:",":champagne:Pezsgőt:champagne_glass:",":whisky:Whiskyt:whisky:",":MGregSip:Kólát:MGregSip:"]
     await ctx.send(random.choice(italok))
 @client.command()
 async def hajrabernatom(ctx):
